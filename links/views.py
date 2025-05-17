78a78f52b2bfbd4241395dfb1eb5ece7a93fc17c
moroshani <moroshaniofficial@gmail.com>
2025-05-17 05:18:13 +0330

    new

--- links/views.py
+++ links/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Link
 
 def home(request):
-    # Simple intro page
     return render(request, 'home.html')
 
 def sites(request):
-    # Fetch all stored links and pass them to the template
-    links = Link.objects.all()
-    return render(request, 'sites.html', {'links': links})
+    queryset = Link.objects.all()
+
+    # --- Search ---
+    query = request.GET.get('q', '').strip()
+    if query:
+        queryset = queryset.filter(url__icontains=query)
+
+    # --- Sorting ---
+    sort = request.GET.get('sort')
+    if sort in ('url', '-url'):
+        queryset = queryset.order_by(sort)
+
+    # --- Pagination (10 per page) ---
+    paginator = Paginator(queryset, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'sites.html', {
+        'page_obj': page_obj,
+        'query': query,
+        'sort': sort,
+    })
