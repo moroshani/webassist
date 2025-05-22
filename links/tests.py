4d9dc0f6e76d97bd5a2ab5a4634275262ea6ee69
moroshani <moroshaniofficial@gmail.com>
2025-05-22 16:55:20 +0330

    test: fix Link tests to provide user and follow redirects for CI

--- links/tests.py
+++ links/tests.py
@@ -1,15 +1,24 @@
 from django.test import TestCase
 from .models import Link
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 class LinkModelTest(TestCase):
     def setUp(self):
-        self.link = Link.objects.create(title="Test", url="https://example.com", description="desc")
+        User = get_user_model()
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+        self.link = Link.objects.create(user=self.user, title="Test", url="https://example.com", description="desc")
 
     def test_link_str(self):
         self.assertEqual(str(self.link), "Test")
 
 class LinkListViewTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(username="testuser2", password="testpass2")
+        self.link = Link.objects.create(user=self.user, title="Test2", url="https://example2.com", description="desc2")
+
     def test_link_list_view_status_code(self):
-        response = self.client.get(reverse('link_list'))
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('link_list'), follow=True)
         self.assertEqual(response.status_code, 200) 
