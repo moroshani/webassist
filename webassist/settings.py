98a63aaf8f0886a7df98a50cdb6f4ba228bd8dd6
moroshani <moroshaniofficial@gmail.com>
2025-05-22 18:01:27 +0330

    fix(db): use dj-database-url for DATABASE_URL and set PGUSER/PGPASSWORD in CI workflow

--- webassist/settings.py
+++ webassist/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 from django.core.exceptions import ImproperlyConfigured
 
@@ -69,11 +70,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "webassist.wsgi.application"
 
+# Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
